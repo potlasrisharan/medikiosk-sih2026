d4ea8ec3c8b832cf6c56406d6f4968c88cd15cf0
potlasrisharan <potlasrisharanchowdary@gmail.com>
2026-08-24 10:21:20 +0530

    fix(all): enable continuous voice appending, document skip button, dynamic AYUSH dashboard, and Admin table crash fix

---
+++ temp.js
@@ -0,0 +1,15 @@
+    tailwind.config = {
+      theme: {
+        extend: {
+          colors: {
+            brand: {
+              navy: "#223D79",
+              grey: "#77797C",
+              white: "#FFFFFF",
+              beige: "#D9CDBA",
+              orange: "#FFA530"
+            }
+          }
+        }
+      }
+    }
